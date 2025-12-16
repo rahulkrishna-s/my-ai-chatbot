9d9466aee7ac8b72f27fac90dab31bbdf521f119
Rahul Krishna S <s.rahulkrishnas@gmail.com>
2025-12-16 10:21:27 -0800

    Connect Gemini API for single turn chat

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 import streamlit as st
+import google.generativeai as genai
 
-st.title("My AI Chatbot")
-st.write("Streamlit is running.")
+st.title("My Gemini Chatbot")
 
-user_input = st.chat_input("Say something:")
+try:
+    genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+    model = genai.GenerativeModel('gemini-2.5-flash')
+    st.success("API Key successfully connected!")
+except Exception as e:
+    st.error("Could not connect to API. Make sure API key is correct.")
+
+
+user_input = st.chat_input("Ask Gemini something...")
+
 if user_input:
-    st.write(f"You said: {user_input}")
+    with st.chat_message("user"):
+        st.write(user_input)
+    
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            try:
+                response = model.generate_content(user_input)
+                st.write(response.text)
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
